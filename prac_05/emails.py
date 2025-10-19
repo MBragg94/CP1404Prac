8d8200c565c33ea12aee792ca522ecac34742c58
MBragg94 <madison.bragg@my.jcu.edu.au>
2025-10-19 13:15:31 +1000

    Add completed code

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -10,3 +10,12 @@ def main():
         email = input("Email: ")
     for email, name in email_to_name.items():
         print(f"{name} ({email})")
+
+def get_name_from_email(email):
+    prefix = email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()
+    return name
+
+
+main()
